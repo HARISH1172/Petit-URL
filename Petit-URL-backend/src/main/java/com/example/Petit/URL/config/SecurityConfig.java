a4a6d7143e8132ff6b2f70b4271d939000fde307
Harish Wagh <waghharish54@gmail.com>
2026-03-11 17:03:06 +0530

    fix CORS

--- Petit-URL-backend/src/main/java/com/example/Petit/URL/config/SecurityConfig.java
+++ Petit-URL-backend/src/main/java/com/example/Petit/URL/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.example.Petit.URL.config;
 
-import com.example.Petit.URL.security.JwtAuthenticationFilter;
-import org.springframework.context.annotation.*;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,7 +15,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.List;
+import com.example.Petit.URL.security.JwtAuthenticationFilter;
 
 @Configuration
 
@@ -53,7 +55,7 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("https://petit-url-six.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
